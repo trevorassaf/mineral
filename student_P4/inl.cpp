ef0f35047338b17de6088a6d60fcbf9e8b6c2a75
Trevor Assaf <trevor.assaf@gmail.com>
2014-04-15 17:35:25 -0400

    initial draft of inl, uncompiled and untested

--- student_P4/inl.cpp
+++ student_P4/inl.cpp
@@ -18,7 +18,114 @@ Status Operators::INL(const string& result,           // Name of the output rela
 {
   cout << "Algorithm: Indexed NL Join" << endl;
 
-  /* Your solution goes here */
+  // Relation names
+  string relation1 = attrDesc1.relName;
+  string relation2 = attrDesc2.relName;
+
+  // Initialize heap file scans
+  Status status;
+  HeapFileScan hfs1(relation1, status);
+  if (status != OK) {
+    return status;
+  }
+  HeapFileScan hfs2(relation2, status);
+  if (status != OK) {
+    return status;
+  }
+
+  // Initialize output heap
+  HeapFile outHfs(result, status);
+  if (status != OK) {
+    return status;
+  }
+
+  // Determine indexed attribute
+  AttrDesc* iaDesc, * niaDesc;
+  HeapFileScan* niHfs, * iHfs;
+  if (attrDesc1.indexed) {
+    iaDesc = &attrDesc1;
+    niaDesc = &attrDesc2;
+    iHfs = &hfs1;
+    niHfs = &hfs2;
+  } else {
+    iaDesc = &attrDesc2;
+    niaDesc = &attrDesc1;
+    iHfs = &hfs2;
+    niHfs = &hfs1;
+  }
+
+  // Initialize index
+  Index index(
+    iaDesc->relName,
+    iaDesc->attrOffset,
+    iaDesc->attrLen,
+    (Datatype)iaDesc->attrType,
+    0,
+    status
+  );
+  if (status != OK) {
+    return status;
+  }
+
+  // Initialize index scan
+  int nihRecCnt = niHfs->getRecCnt();
+  for (int nihIdx = 0; nihIdx < nihRecCnt; ++nihIdx) {
+    RID niRid;
+    Record niRecord;
+    status = niHfs->scanNext(rid, niRecord);
+    if (status == FILEEOF) {
+      break;
+    }
+    if (status != OK) {
+      return status;
+    }
+
+    // Search through index to find record to join on 
+    // TODO: what if there are multiple records to join on?? Join on all of them, right?
+    index.startScan(niRecord.data);
+    int ihRecCnt = iHfs->getRecCnt();
+    for (int iIdx = 0; iIdx < ihRecCnt; ++iIdx) {
+      RID iRid;
+      status = index.scanNext(iRid);
+      if (status == NOMORERECS) {
+        break;
+      }
+      if (status != OK) {
+        return status;
+      }
+
+      // Fetch join record from heap file
+      Record iRecord;
+      status = iHfs->getRandomRecord(iRid, iRecord);
+      if (status != OK) {
+        return status;
+      }
+
+      // Join attributes in new memory block
+      char* newRecordMem = new char[reclen];
+      for (int pIdx = 0; pIdx < projCnt; ++pIdx) {
+        Record* inputRecord = strcmp(
+            attrDescArray[pIdx].relName,
+            iaDesc->relName)
+          ? iRecord
+          : niRecord;
+        memcpy(
+          newRecordMem,
+          inputRecord->data + attrDescArray[pIdx].attrOffset,
+          attrDescArray[pIdx].attrLen);
+        newRecordMem += attrDescArray[pIdx].attrLen;
+      }
+      newRecordMem -= reclen;
+
+      // Insert join record into results relation
+      RID outRid;
+      Record outRec = {newRecordMem, reclen};
+      status = outHfs.insertRecord(outRec, outRid);
+      if (status != OK) {
+        return status;
+      }
+    }
+  }
 
   return OK;
 }
