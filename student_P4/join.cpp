78a89a83b0ee9a7fd8b6b058a783bf8729411ad5
Hussein <hnagree@caen-vnc02.engin.umich.edu>
2014-04-15 02:20:00 -0400

    Join

--- student_P4/join.cpp
+++ student_P4/join.cpp
@@ -4,6 +4,8 @@
 #include "index.h"
 #include <cmath>
 #include <cstring>
+#include <string>
+#include <cassert>
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define DOUBLEERROR 1e-07
@@ -23,9 +25,99 @@ Status Operators::Join(const string& result,           // Name of the output rel
     	               const Operator op,              // Predicate operator
     	               const attrInfo* attr2)          // Right attr in the join predicate
 {
-    /* Your solution goes here */
-
-	return OK;
+  // Fetch attribute descriptions
+  string relation1 = attr1->relName;
+  string relation2 = attr2->relName;
+  
+  AttrDesc* inputDescs1, * inputDescs2;
+  
+  int attrCnt1, attrCnt2;
+  
+  Status status = attrCat->getRelInfo(relation1, attrCnt1, inputDescs1);
+  if (status != OK) {
+    return status;
+  }
+  status = attrCat->getRelInfo(relation2, attrCnt2, inputDescs2);
+  if (status != OK) {
+    return status;
+  }
+  
+  // Construct list of projected attribute descriptions
+  AttrDesc* projAttrDescs = new AttrDescs[projCnt];
+  int reclen = 0;
+  for (int i = 0; i < projCnt; ++i) {
+    bool found = false;
+    int j;
+    for (j = 0; j < attrCnt1; ++j) {
+      if (!strcmp(projNames[i].attrName, inputDescs1->attrName) && 
+          !strcmp(projNames[i].relName, inputDescs1->relName)) {
+        *projAttrDescs++ = *inputDescs1;  
+        found = true;
+        break;
+      } 
+    }
+    inputDescs1 -= j;
+    
+    if (found) {
+      continue;
+    }
+    
+    for (j = 0; j < attrCnt2; ++j) {
+      if (!strcmp(projNames[i].attrName, inputDescs2->attrName) && 
+          !strcmp(projNames[i].relName, inputDescs2->relName)) {
+        *projAttrDescs++ = *inputDescs2;  
+        found = true;
+        break;
+      } 
+    }
+    inputDescs2 -= j;
+    assert(found); 
+    reclen += (projAttrDescs - 1)->attrLen;
+  }
+  projAttrDescs -= projCnt;
+  
+  // Convert criterion attributes to AttrDesc
+  AttrDesc cDesc1, cDesc2;
+  status = attrCat->getInfo(relation1, attr1->attrName, cDesc1);
+  if (status != OK) {
+    return status;
+  }
+  status = attrCat->getInfo(relation2, attr2->attrName, cDesc2);
+  if (status != OK) {
+    return status;
+  } 
+  
+  // Delegate sub-join operations
+  if (op != EQ) {
+    return SNL(
+      result,
+      projCnt,
+      projAttrDescs,
+      cDesc1,
+      op,
+      cDesc2,
+      reclen);
+  }
+  
+  if (cDesc1->indexed || cDesc2->indexed) {
+    return INL(
+      result,
+      projCnt,
+      projAttrDescs,
+      cDesc1,
+      op,
+      cDesc2,
+      reclen);  
+  }
+  
+  return SMJ(
+      result,
+      projCnt,
+      projAttrDescs,
+      cDesc1,
+      op,
+      cDesc2,
+      reclen);
 }
 
 // Function to compare two record based on the predicate. Returns 0 if the two attributes 
