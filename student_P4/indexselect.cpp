84112c1dbb7df2feee38700e7e10171342dac046
Hussein <hnagree@caen-vnc02.engin.umich.edu>
2014-04-14 23:49:35 -0400

    Scan done

--- student_P4/indexselect.cpp
+++ student_P4/indexselect.cpp
@@ -1,6 +1,8 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include <cstring>
+
 Status Operators::IndexSelect(const string& result,       // Name of the output relation
                               const int projCnt,          // Number of attributes in the projection
                               const AttrDesc projNames[], // Projection list (as AttrDesc)
@@ -10,8 +12,67 @@ Status Operators::IndexSelect(const string& result,       // Name of the output 
                               const int reclen)           // Length of a tuple in the output relation
 {
   cout << "Algorithm: Index Select" << endl;
-
-  /* Your solution goes here */
+  
+  // Initialize index
+  string relation = projNames[0].relName;
+  Status status;
+  Index index(
+    relation,
+    attrDesc->attrOffset,
+    attrDesc->attrLen,
+    (Datatype)attrDesc->attrType,
+    0,
+    status);
+  if (status != OK) {
+    return status;
+  }
+  
+  // Initizlize relation heap file scan
+  HeapFileScan inputHfs(relation, status);
+  if (status != OK) {
+    return status;
+  }
+  
+  // Initialize results heap file
+  HeapFile outputHfs(result, status);
+  
+  // Scan index for next RIDs
+  int numRecs = inputHfs.getRecCnt();
+  for (int i = 0; i < numRecs; ++i) {
+    // Fetch rid of next qualifying record
+    RID rid;
+    status = index.scanNext(rid);
+    if (status == NOMORERECS) {
+      break;
+    } else if (status != OK) {
+      return status;
+    }
+    
+    // Fetch record from input heap file with rid
+    Record inputRecord;
+    status = inputHfs.getRecord(rid, inputRecord);
+    if(status != OK) {
+      return status;
+    }
+      
+    // Project attributes into new record
+    char* projData = new char[reclen];
+    int offset = 0; 
+    for (int j = 0; j < projCnt; ++j) {
+      memcpy(
+        projData + offset,
+        (char*) inputRecord.data + projNames[j].attrOffset,
+        projNames[j].attrLen);
+      offset += projNames[j].attrLen;
+    }
+    Record resultsRecord = {projData, reclen};
+    
+    // Add new record to results heap file
+    status = outputHfs.insertRecord(resultsRecord, rid);
+    if (status != OK) {
+      return status;
+    }
+  }
 
   return OK;
 }
--- student_P4/scanselect.cpp
+++ student_P4/scanselect.cpp
@@ -1,6 +1,7 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include <cstring>
 
 /* 
  * A simple scan select using a heap file scan
@@ -16,7 +17,60 @@ Status Operators::ScanSelect(const string& result,       // Name of the output r
 {
   cout << "Algorithm: File Scan" << endl;
   
-  /* Your solution goes here */
+  // Initialize heap file scan
+  string relation = projNames[0].relName;
+  Status status;
+  HeapFileScan hfs = attrDesc 
+    ? HeapFileScan(
+      relation,
+      attrDesc->attrOffset,
+      attrDesc->attrLen,
+      (Datatype)attrDesc->attrType,
+      (const char*)attrValue,
+      op,
+      status) 
+    : HeapFileScan(relation, status); 
+  if (status != OK) {
+    return status;
+  }
+  
+  // Obtain results heap file
+  HeapFile outHf(result, status);
+  if (status != OK) {
+    return status;
+  }
+    
+  // Perform record-wise scan
+  int numRecCnt = hfs.getRecCnt();
+  for (int i = 0; i < numRecCnt; ++i) {
+    // Obtain record satisfying selection criterion
+    RID rid;
+    Record record;
+    status = hfs.scanNext(rid, record);
+    if (status == FILEEOF) {
+      break;
+    } else if (status != OK) {
+      return status;
+    }
+    
+    // Project attributes into new record
+    char* projData = new char[reclen];
+    int offset = 0; 
+    for (int j = 0; j < projCnt; ++j) {
+      memcpy(
+        projData + offset,
+        (char *) record.data + projNames[j].attrOffset,
+        projNames[j].attrLen);
+      offset += projNames[j].attrLen;
+    }
+    Record resultsRecord = {projData, reclen};
+    
+    // Add new record to results heap file
+    status = outHf.insertRecord(resultsRecord, rid); // we don't care about this rid
+    if (status != OK) {
+      return status;
+    }
+  }
 
   return OK;
 }
